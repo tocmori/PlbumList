b0cd2a30b59fd4a8a53382d318994f5cec8127a0
Kenta Omori <omori.kenta@gmail.com>
2010-05-06 16:02:17 +0900

    Commit Hello World code.

---
+++ src/jp/tocmori/plbumlist/PlbumList.java
@@ -0,0 +1,13 @@
+package jp.tocmori.plbumlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlbumList extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
